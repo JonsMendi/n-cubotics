8099ba4d8842b12fc17c1148faf9298d7338ff1c
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-04-07 17:59:43 +0200

    main: added baud rate dropdown and sync values with angle

--- pages/page-components/cube.tsx
+++ pages/page-components/cube.tsx
@@ -1,28 +1,30 @@
 import * as THREE from 'three';
-import { useRef } from 'react';
+import { useRef, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
+import React from 'react';
 
 type CubeProps = {
   angle: number;
+  antialias?: boolean;
+  shadows?: boolean;
 };
 
-const Cube = ({ angle }: CubeProps) => {
+const Cube = ({ angle, ...rest }: CubeProps) => {
   const cubeAnimationRef = useRef<THREE.Mesh>(null) as React.MutableRefObject<THREE.Mesh>;
+  // Texture from image
+  const texture = useMemo(() => new THREE.TextureLoader().load('/metal-gold.jpg'), []);
 
   useFrame(({ camera }) => {
     const cubeAnimation = cubeAnimationRef.current;
     if (cubeAnimation) {
       cubeAnimation.rotation.z = THREE.MathUtils.degToRad(angle);
-      // Update camera position and rotation
+      // Update camera position and rotation to Right-Hand coordinate system (+x -> forward, +y ->left, +z -> up)
       camera.position.set(0, 8, 0);
       camera.up.set(0, 0, 1);
       camera.lookAt(cubeAnimation.position);
     }
   });
-
-  // Texture from image
-  const texture = new THREE.TextureLoader().load('/metal-gold.jpg');
 
   return (
     <>
@@ -35,19 +37,4 @@ const Cube = ({ angle }: CubeProps) => {
   );
 };
 
-export default Cube;
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
+export default React.memo(Cube);
--- pages/page-components/dashboard.tsx
+++ pages/page-components/dashboard.tsx
@@ -1,17 +1,69 @@
 import { Canvas } from '@react-three/fiber';
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import Cube from './cube';
 
+const baudRateOptions = ["9600", "19200", "38400", "57600", "115200", ""];
+
 const Dashboard = () => {
-  const [angle, setAngle] = useState(190);
+  const [angle, setAngle] = useState(50);
+  console.log("🚀 ~ file: dashboard.tsx:9 ~ Dashboard ~ angle:", angle)
+  const [baudRate, setBaudRate] = useState<string>(
+    typeof localStorage !== 'undefined' ? localStorage.getItem('baudRate') || baudRateOptions[0] : baudRateOptions[0]
+  );
+
+  const handleBaudRateChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newBaudRate = event.target.value;
+    if (newBaudRate === "") {
+      // Set the angle to the current rotation angle when the "Stop Rotation" option is selected
+      setAngle(angle);
+    }
+    setBaudRate(newBaudRate);
+    localStorage.setItem('baudRate', newBaudRate);
+  };
+
+  // Update angle in response to changes in baud rate
+  useEffect(() => {
+    if (baudRate === '') {
+      return;
+    }
+    let rotationIncrement = 360 / parseInt(baudRate);
+    let direction = 1;
+    const intervalId = setInterval(() => {
+      setAngle(angle => {
+        let newAngle = angle + rotationIncrement * direction;
+        if (newAngle > 360) {
+          newAngle -= 360;
+        } else if (newAngle < 0) {
+          newAngle += 360;
+        }
+        // Restart rotation direction if angle is at 0 or 360 degrees
+        if (newAngle === 0 || newAngle === 360) {
+          direction *= -1;
+        }
+        return newAngle;
+      });
+    }, 1000 / parseInt(baudRate));
+
+    return () => clearInterval(intervalId);
+  }, [baudRate]);
 
   return (
     <div style={{ width: '100%', height: '100vh' }}>
+      <div>
+        <label htmlFor="baudRate">Baud Rate:</label>
+        <select id="baudRate" name="baudRate" value={baudRate} onChange={handleBaudRateChange}>
+          {baudRateOptions.map((option) => (
+            <option key={option} value={option}>
+              {option !== "" ? option : 'Stop Rotation'}
+            </option>
+          ))}
+        </select>
+      </div>
       <Suspense fallback={null}>
         <Canvas shadows>
-          <ambientLight intensity={0.5}/>
+          <ambientLight intensity={0.5} />
           <pointLight position={[2, 5, 2]} intensity={1} />
-            <Cube angle={angle} />
+          <Cube angle={angle} />
         </Canvas>
       </Suspense>
     </div>
@@ -19,5 +71,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-
