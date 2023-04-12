03101c2a66dae45ae0cebb2cf26200841219d41a
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-04-12 17:39:50 +0200

    main: add Tween.js for animation

--- pages/page-components/cube.tsx
+++ pages/page-components/cube.tsx
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
-import { useRef, useMemo } from 'react';
+import { useRef, useMemo, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
-import React from 'react';
+import * as TWEEN from '@tweenjs/tween.js';
 
 type CubeProps = {
   angle: number;
@@ -10,15 +10,50 @@ type CubeProps = {
   shadows?: boolean;
 };
 
+interface Rotation {
+  z: number;
+}
+
+/**
+ * The Cube class
+ * @param param0 
+ * @returns - the Cube object
+ */
 const Cube = ({ angle, ...rest }: CubeProps) => {
-  const cubeAnimationRef = useRef<THREE.Mesh>(null) as React.MutableRefObject<THREE.Mesh>;
-  // Texture from image
+  const cubeAnimationRef = useRef<THREE.Mesh>(null);
+  const currentRotationRef = useRef(0);
+  const tweenRef = useRef<TWEEN.Tween<Rotation> | null>(null);
   const texture = useMemo(() => new THREE.TextureLoader().load('/metal-gold.jpg'), []);
 
+  useEffect(() => {
+    if (cubeAnimationRef.current && currentRotationRef.current !== angle) {
+      if (tweenRef.current) {
+        tweenRef.current.stop();
+      }
+
+      const startRotation = { z: cubeAnimationRef.current.rotation.z };
+      const targetRotation = { z: -THREE.MathUtils.degToRad(angle) };
+
+      // create animation with Tween.js for smooth rotation
+      tweenRef.current = new TWEEN.Tween(startRotation)
+        .to(targetRotation, 1000)
+        .easing(TWEEN.Easing.Sinusoidal.Out)
+        .onUpdate(() => {
+          if (cubeAnimationRef.current) {
+            cubeAnimationRef.current.rotation.z = startRotation.z;
+          }
+        })
+        .start();
+      currentRotationRef.current = angle;
+    }
+  }, [angle]);
+
+
   useFrame(({ camera }) => {
+    TWEEN.update();
     const cubeAnimation = cubeAnimationRef.current;
+    
     if (cubeAnimation) {
-      cubeAnimation.rotation.z = -THREE.MathUtils.degToRad(angle);
       // Update camera position and rotation to Right-Hand coordinate system (+x -> forward, +y ->left, +z -> up)
       camera.position.set(0, 8, 0);
       camera.up.set(0, 0, 1);
@@ -29,7 +64,7 @@ const Cube = ({ angle, ...rest }: CubeProps) => {
   return (
     <>
       <PerspectiveCamera makeDefault fov={50} />
-      <mesh ref={cubeAnimationRef}>
+      <mesh rotation={[0, 0, Math.PI / 4]} ref={cubeAnimationRef}>
         <boxGeometry args={[2, 2, 2]} />
         <meshBasicMaterial map={texture} />
       </mesh>
@@ -37,4 +72,4 @@ const Cube = ({ angle, ...rest }: CubeProps) => {
   );
 };
 
-export default React.memo(Cube);
+export default Cube;
