54443d6cce1d3ec06d6aeed0411c02d8caecad2e
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-04-15 23:02:36 +0200

    main: trying to deploy in gh-p

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.child_process = false;
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
